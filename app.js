288209180cbb4280245b02acb1cb10e3754d85d0
Math1299 <perret.mathieu@bluewin.ch>
2021-01-19 14:03:20 +0100

    structure globale ok

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const modaleEquipement = document.querySelector(".bloc-modale");
 const fermetureModale = document.querySelector(".fermeture-modale");
 const imgIndex = document.querySelector(".bloc-modale img");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    imgIndex.src = `ressources/img${e.target.getAttribute(
-      "data-index"
-    )}-equip.jpg`;
-    modaleEquipement.classList.add("active-modale");
+if (window.matchMedia("(min-width: 850px)").matches) {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      imgIndex.src = `ressources/img${e.target.getAttribute(
+        "data-index"
+      )}-equip.jpg`;
+      modaleEquipement.classList.add("active-modale");
+    });
   });
-});
 
-modaleEquipement.addEventListener("click", () => {
-  modaleEquipement.classList.remove("active-modale");
+  modaleEquipement.addEventListener("click", () => {
+    modaleEquipement.classList.remove("active-modale");
+  });
+}
+
+// Anim Navbar
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
+    nav.classList.add("anim-nav");
+  } else {
+    nav.classList.remove("anim-nav");
+  }
 });
